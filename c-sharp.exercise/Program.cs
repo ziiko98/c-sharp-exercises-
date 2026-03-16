924df69f5a28cd6060ee49a54a386d6977296767
ziiko98 <zeinnaab@hotmail.com>
2026-03-16 13:14:24 +0100

    Add Program.cs

---
+++ c-sharp.exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+    
+// C# methods - 1
+    Console.WriteLine("Enter your password");
+    string password = Console.ReadLine();
+    bool passwordMatch = CheckPassword(password);
+    if (passwordMatch)
+    {
+        Console.WriteLine("Success");
+    }
+    else
+    {
+        Console.WriteLine("Login failed");
+    }
+    
+    bool CheckPassword(string password)
+    {
+    return password.ToLower() == "pencil";
+    }
+
